c18535911f42900baf8980c7f0f2a713cd800757
arthurolivierfortin <arthurolivier.fortin@gmail.com>
2025-11-09 22:13:19 +0000

    Made WebSockets work

--- PiCarWebSockets.py
+++ PiCarWebSockets.py
@@ -1,19 +1,37 @@
 import asyncio
 import websockets
+import json
+from PiCarFunctions import PiCarFunctions
 
 async def receivesAndSend(websocket):
-    # Receives
-    infoReceived = await websocket.recv()
-    print(f'Receiving: {infoReceived}')
+    print("Connection has been made with GoDot!")
 
-    # Send
-    infoSending = f'Hi, I just received this: {infoReceived}!'
-    print(f'Sending: {infoSending}')
-    await websocket.send(infoSending)
+    try:
+        async for message in websocket:
+            print(f'Receiving: {message}')
+
+            try:
+                data = json.loads(message)
+                velocity = data.get("velocity", 0)
+                direction = data.get("direction", 0)
+                print(f'Velocity: {velocity}, Direction: {direction}')
+            except json.JSONDecodeError:
+                print(f'Received non-JSON: {message}')
+
+            response = {
+                "distance": 100,
+                "line_follower": [1, 0, 1, 0, 1]
+            }
+            await websocket.send(json.dumps(response))
+            print(f'Sent: {response}')
+
+    except websockets.exceptions.ConnectionClosed:
+        print("Godot client disconnected")
 
 async def main():
-  async with websockets.serve(receivesAndSend, "localhost", 8765):
-     await asyncio.Future()
+    async with websockets.serve(receivesAndSend, "0.0.0.0", 8765):
+        print("WebSocket server running on ws://0.0.0.0:8765")
+        await asyncio.Future()
 
-if __name__ == "main":
-   asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
